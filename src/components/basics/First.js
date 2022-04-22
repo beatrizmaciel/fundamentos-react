e13bd39aa5dd01ce19b2ac7de65c31dd66ca7350
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-04-22 17:58:25 -0300

    creating const

--- src/components/basics/First.js
+++ src/components/basics/First.js
@@ -1,3 +1,13 @@
+import React from 'react'
+
 export default function First() {
-    return 'Primeiro Componente React'
+    
+    const msg = 'Seja bem vindo(a)!'
+    
+    return (
+        <div>
+            <h2>Primeiro Componente ff</h2>
+            <p>{msg}</p>
+        </div>
+    )
 }
